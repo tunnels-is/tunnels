bdce74495459dee5d564b5e75531da6dfcb1067f
zveinn <info@sveinn.is>
2024-10-13 17:02:17 +0000

    Adding the server

--- go.mod
+++ go.mod
@@ -8,9 +8,12 @@ require (
 	github.com/jackpal/gateway v1.0.15
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/miekg/dns v1.1.62
+	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/vishvananda/netlink v1.3.0
 	github.com/xlzd/gotp v0.1.0
 	github.com/zveinn/crypt v0.0.1
+	github.com/zveinn/tunnels v0.0.4
+	go.mongodb.org/mongo-driver v1.17.1
 	golang.org/x/net v0.30.0
 	golang.org/x/sys v0.26.0
 	golang.org/x/term v0.25.0
@@ -19,6 +22,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -26,9 +30,12 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.14 // indirect
+	github.com/tklauser/numcpus v0.8.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/vishvananda/netns v0.0.4 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/mod v0.18.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
---
+++ server/structs.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"io"
+	"net"
+	"time"
+
+	"github.com/zveinn/crypt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/net/quic"
+)
+
+var (
+	Config                       = new(Server)
+	InterfaceIP                  net.IP
+	PortMappingResponseDurations = time.Duration(30 * time.Second)
+	serverConfigPath             = "./server.json"
+	publicPath                   string
+	privatePath                  string
+	publicSigningCert            *x509.Certificate
+	publicSigningKey             *rsa.PublicKey
+	controlCertificate           tls.Certificate
+	controlConfig                *tls.Config
+	quicConfig                   *quic.Config
+	dataSocketFD                 int
+	rawUDPSockFD                 int
+	rawTCPSockFD                 int
+	slots                        int
+
+	TCPRWC                 io.ReadWriteCloser
+	UDPRWC                 io.ReadWriteCloser
+	toUserChannelMonitor   = make(chan int, 10000)
+	fromUserChannelMonitor = make(chan int, 10000)
+)
+
+type Server struct {
+	ID                 primitive.ObjectID `json:"ID"`
+	ControlIP          string             `json:"ControlIP"`
+	ControlPort        string             `json:"ControlPort"`
+	UserMaxConnections int                `json:"UserMaxConnections"`
+	ProxyAddress       string             `json:"ProxyIP"`
+	InterfaceIP        string             `json:"InterfaceIP"`
+	DataPort           string             `json:"DataPort"`
+	StartPort          int                `json:"StartPort"`
+	EndPort            int                `json:"EndPort"`
+	AvailableMbps      int                `json:"AvailableMbps"`
+	AvailableUserMbps  int                `json:"AvailableUserMbps"`
+	InternetAccess     bool               `json:"InternetAccess,required"`
+	LocalNetworkAccess bool               `json:"LocalNetworkAccess"`
+	DNSAllowCustomOnly bool               `json:"DNSAllowCustomOnly"`
+	DNS                []*ServerDNS       `json:"DNS"`
+	Networks           []*ServerNetwork   `json:"Networks"`
+	DNSServers         []string           `json:"DNSServers"`
+
+	AutoCert    bool   `json:"AutoCert"`
+	Cert        *Cert  `json:"Cert"`
+	ControlCert string `json:"ControlCert"`
+	ControlKey  string `json:"ControlKey"`
+	SignKey     string `json:"SignKey"`
+}
+
+type Cert struct {
+	IPs     []string  `json:"IPs"`
+	Domains []string  `json:"Domains"`
+	Org     string    `json:"Org"`
+	Expires time.Time `json:"Expires"`
+}
+
+type ServerDNS struct {
+	Domain   string   `json:"Domain" bson:"Domain"`
+	Wildcard bool     `json:"Wildcard" bson:"Wildcard"`
+	IP       []string `json:"IP" bson:"IP"`
+	TXT      []string `json:"TXT" bson:"TXT"`
+	CNAME    string   `json:"CNAME" bson:"CNAME"`
+}
+
+type ServerNetwork struct {
+	Tag     string   `json:"Tag" bson:"Tag"`
+	Network string   `json:"Network" bson:"Network"`
+	Nat     string   `json:"Nat" bson:"Nat"`
+	Routes  []*Route `json:"Routes" bson:"Routes"`
+}
+
+type Route struct {
+	Address string
+	Metric  string
+}
+
+type ConnectRequestResponse struct {
+	Index             int `json:"Index"`
+	AvailableMbps     int `json:"AvailableMbps"`
+	AvailableUserMbps int `json:"AvailableUserMbps"`
+
+	InternetAccess     bool `json:"InternetAccess,required"`
+	LocalNetworkAccess bool `json:"LocalNetworkAccess"`
+
+	InterfaceIP string `json:"InterfaceIP"`
+	DataPort    string `json:"DataPort"`
+	StartPort   uint16 `json:"StartPort"`
+	EndPort     uint16 `json:"EndPort"`
+
+	DNS                []*ServerDNS     `json:"DNS"`
+	Networks           []*ServerNetwork `json:"Networks"`
+	DNSServers         []string         `json:"DNSServers"`
+	DNSAllowCustomOnly bool             `json:"DNSAllowCustomOnly"`
+}
+
+func CreateCRRFromServer(S *Server) (CRR *ConnectRequestResponse) {
+	return &ConnectRequestResponse{
+		Index:              0,
+		StartPort:          0,
+		EndPort:            0,
+		DataPort:           S.DataPort,
+		AvailableMbps:      S.AvailableMbps,
+		AvailableUserMbps:  S.AvailableUserMbps,
+		InternetAccess:     S.InternetAccess,
+		LocalNetworkAccess: S.LocalNetworkAccess,
+		InterfaceIP:        S.InterfaceIP,
+		DNS:                S.DNS,
+		Networks:           S.Networks,
+		DNSServers:         S.DNSServers,
+		DNSAllowCustomOnly: S.DNSAllowCustomOnly,
+	}
+}
+
+type ConnectRequest struct {
+	DeviceToken string             `json:"DeviceToken"`
+	EncType     crypt.EncType      `json:"EncType"`
+	UserID      primitive.ObjectID `json:"UserID"`
+	SeverID     primitive.ObjectID `json:"ServerID"`
+	Serial      string             `json:"Serial"`
+
+	Version int       `json:"Version"`
+	Created time.Time `json:"Created"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"Error"`
+}
