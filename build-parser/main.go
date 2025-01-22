3c571b0451e1515e12c7f3ff210deff163b4e57d
zveinn <info@sveinn.is>
2025-01-22 17:48:46 +0000

    Adding a parser to catch debug logs

---
+++ build-parser/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	filter   = []byte("fmt.Println")
+	fmtCount = 0
+)
+
+func main() {
+	checkDir("../server")
+	checkDir("../core")
+	checkDir("../certs")
+	checkDir("../iptables")
+	checkDir("../setcap")
+	if fmtCount > 0 {
+		panic("YOU HAVE DEBUG PRINTS IN THE BUILD")
+	}
+}
+
+func checkDir(dir string) {
+	filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if strings.HasSuffix(path, ".crt") {
+			return nil
+		}
+		if strings.HasSuffix(path, ".dll") {
+			return nil
+		}
+		if strings.HasSuffix(path, ".json") {
+			return nil
+		}
+		if strings.HasSuffix(path, "server") {
+			return nil
+		}
+
+		fmt.Println(path)
+		fb, err := os.ReadFile(path)
+		fbs := bytes.Split(fb, []byte{10})
+		for i := range fbs {
+			if bytes.Contains(fbs[i], filter) {
+				if path == "../core/logging.go" && i == 177 {
+					if bytes.Contains(fbs[i], []byte("fmt.Println(line)")) {
+						continue
+					}
+				}
+				fmt.Println(i, string(fbs[i]))
+				fmtCount++
+			}
+		}
+		return nil
+	})
+}
