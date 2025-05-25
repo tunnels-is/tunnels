afe9ce8c2f9741458002d91c4bc1572c8b16451e
zveinn <info@sveinn.is>
2025-05-25 13:57:37 +0000

    ff

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 
+	"github.com/tunnels-is/tunnels/client"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -17,6 +18,7 @@ var assets embed.FS
 func main() {
 	// Create an instance of the app structure
 	app := NewApp()
+	apiBridge := &client.APIBridge{}
 
 	// Create application with options
 	err := wails.Run(&options.App{
@@ -30,6 +32,7 @@ func main() {
 		OnStartup:        app.startup,
 		Bind: []any{
 			app,
+			apiBridge,
 		},
 	})
 
