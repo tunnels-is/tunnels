691ea515d7238f598d8ef05ea2cf1869a347a4e6
zveinn <info@sveinn.is>
2025-06-22 10:55:34 -0500

    updating build scripts

--- releaser-build-release.sh
+++ releaser-build-release.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # export GITHUB_TOKEN=$1
 goreleaser release --clean
-cp builds/server_linux_amd64_v1/tunnels devops/build/
--- releaser-build-snapshot.sh
+++ releaser-build-snapshot.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 goreleaser build --snapshot --clean
-cp builds/server_linux_amd64_v1/tunnels devops/build/
-
