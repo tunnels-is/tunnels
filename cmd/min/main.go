d9477587db82f99115dcc73aa2dff9733d117ee5
zveinn <info@sveinn.is>
2025-06-27 13:08:35 +0000

    testing out some auto update

--- cmd/min/main.go
+++ cmd/min/main.go
@@ -9,15 +9,23 @@ import (
 
 func main() {
 	cli := client.CLIConfig.Load()
+	var disableAutoUpdate bool
+	
 	flag.StringVar(&cli.AuthServer, "authHost", "api.tunnels.is", "The auth server you want to use")
 	flag.StringVar(&cli.DeviceID, "deviceID", "", "the device token")
 	flag.StringVar(&cli.ServerID, "serverID", "", "the server you want to connect to")
 	// flag.BoolVar(&cli.DNS, "dns", false, "enable dns server")
 	flag.BoolVar(&cli.Secure, "secure", false, "validate TLS certificate")
 	flag.BoolVar(&cli.SendStats, "sendStats", true, "send device statistics")
+	flag.BoolVar(&disableAutoUpdate, "disableAutoUpdate", false, "disable automatic updates")
 	cli.Enabled = true
 	client.CLIConfig.Store(cli)
 	flag.Parse()
+
+	// Check for updates before starting the service (unless disabled)
+	if !disableAutoUpdate {
+		AutoUpdate()
+	}
 
 	// device, err := client.GetDeviceByID(cli.Secure, cli.AuthServer, cli.DeviceID)
 	// if err != nil {
